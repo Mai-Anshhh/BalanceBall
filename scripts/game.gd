a7126b0eda671cf9c6bc28ddb20d40e4b9445678
Mai-Anshhh <rajs002ssp@gmail.com>
2026-05-17 17:11:21 +0530

    Added highscore and some graphics
    added a highscore system to the game, and some assets to make it look better

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,21 @@
 extends Node2D
 
-var score = 0
 @onready var timer: Timer = $Timer
 @onready var scoredisplay: Label = $Control/scoredisplay
+@onready var highscore_label: Label = $Control/HighscoreLabel
 
+func _ready():
+	Global.score = 0
+	Global.load_highscore()
+	update_ui()
 
-func _on_timer_timeout() -> void:
-	score += 5
-	$Control/scoredisplay.text = "Score: " + str(score)
+func _on_timer_timeout():
+	Global.score += 5
+	if Global.score > Global.highscore:
+		Global.highscore = Global.score
+		Global.save_highscore()
+	update_ui()
+
+func update_ui():
+	scoredisplay.text = "Score: " + str(Global.score)
+	highscore_label.text = "Best: " + str(Global.highscore)
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
+@onready var highscore: Label = $highscore
 
+func _ready():
+	Global.load_highscore()
+	update_ui()
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/game.tscn")
+
+func update_ui():
+	highscore.text = "Best: " + str(Global.highscore)
